cd38226d53f2b5c62ce1de54db60f678a8d9d78f
khouni <khouni1974@hotmail.com>
2017-10-03 18:20:12 -0400

    take pictures

--- app/src/main/java/com/example/khouni/trainerpro/TakePicture.java
+++ app/src/main/java/com/example/khouni/trainerpro/TakePicture.java
@@ -1,17 +1,55 @@
 package com.example.khouni.trainerpro;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class TakePicture extends AppCompatActivity {
-
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
+        button = (Button) findViewById(R.id.tpButton);
+        imageView = (ImageView)findViewById(R.id.CustImageView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+
+
+            }
+        });
+    }
+    private File getFile()
+    {
+    File folder = new File("sdcard/camera_app");
+        if(!folder.exists())
+        {
+        folder.mkdir();
+        }
+        File image_file = new File(folder,"cam_image.jpg");
+        return image_file;
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
